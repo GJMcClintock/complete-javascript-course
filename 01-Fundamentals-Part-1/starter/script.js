f9e9f28c97b9b10765fcdd7289964a77c25442cf
Garrett Mcclintock <garrettjmcclintock@gmail.com>
2022-01-17 23:17:10 -0700

    lectures

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let country = 'USA';
+let continent = 'NA';
+let population = 320000000;
+
+console.log(country);
+console.log(continent);
+console.log(population)
